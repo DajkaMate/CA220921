f93547bcda19e6caa8d306df903bf6545e0470aa
DajkaMate <72004720+DajkaMate@users.noreply.github.com>
2022-09-21 09:40:38 +0200

    Add files via upload

---
+++ CA220921/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using CA220921;
+
+List <Kuldetes> kuldetesek = new List <Kuldetes> ();
+
+using StreamReader sr = new(@"..\..\..\res\kuldetesek.csv");
+while (!sr.EndOfStream) kuldetesek.Add(new Kuldetes(sr.ReadLine()));
+
+//3. feladat 
+Console.WriteLine("$*3.feladat:\n\t" + 
+    $"Összesen {kuldetesek.Count} alkalommal indítottak űrsiklót");
+
+//4. feladat
+
+
+
